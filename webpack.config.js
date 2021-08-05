72069ead1b7a27704058c26a2a94c2963f9448f1
Serdar <serdaratamuradov24@gmail.com>
2021-08-05 12:16:21 +0300

    added eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const jsLoaders = () => {
+  const loaders = ['babel-loader']
+
+  if (isDev) loaders.push('eslint-loader')
+
+  return loaders
+}
+
 module.exports = {
-  entry: __dirname + "/src/app/index.js",
+  entry: __dirname + '/src/app/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: jsLoaders(),
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + "/src/public/index.html",
-      inject: "body",
+      template: __dirname + '/src/public/index.html',
+      inject: 'body',
     }),
   ],
   devServer: {
-    contentBase: "./src/public",
+    contentBase: './src/public',
     port: 7700,
     historyApiFallback: true,
   },
-};
+}
