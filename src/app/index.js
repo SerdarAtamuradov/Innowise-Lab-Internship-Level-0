6357e3ecde2a37302c543611aa18e9f90639e5cf
Serdar <serdaratamuradov24@gmail.com>
2021-08-09 10:02:35 +0300

    added logic to e number

--- src/app/index.js
+++ src/app/index.js
@@ -28,40 +28,6 @@ tables.onclick = function (event) {
   historyExpressionElem.innerText = historyExpression
 }
 
-function handleOperation(action) {
-  symbol = handleActionSymbols(action)
-
-  if (lastAction === 'equal') {
-    historyExpression = result + symbol
-    expressionString = result.toString() + symbol
-    console.log('expressionString', expressionString)
-
-    currentExpression = ''
-    lastAction = action
-    canPutDot = true
-    return
-  }
-
-  if ((lastAction == 'divide' || lastAction == 'recall-memory') && currentExpression == '0') {
-    console.log(memory)
-    console.log(currentExpression)
-    historyExpression += currentExpression + ' ='
-    currentExpression = 'На ноль делить нельзя'
-    expressionString = ''
-    return
-  }
-
-  if (action == 'equal') symbol = ''
-  expressionString += currentExpression + symbol
-
-  historyExpression = expressionString
-  if (action == 'equal') historyExpression += ' ='
-
-  currentExpression = ''
-  lastAction = action
-  canPutDot = true
-}
-
 const handleClick = (action, value) => {
   switch (action) {
     case 'add':
@@ -76,48 +42,72 @@ const handleClick = (action, value) => {
     case 'divide':
       handleOperation(action)
       break
-    case 'equal':
+    case 'equal': {
+      console.log(currentExpression)
+      console.log(lastAction)
       if (!currentExpression || lastAction === 'equal') {
         historyExpression = currentExpression
         break
       }
       handleOperation(action)
       result = eval(expressionString)
-      console.log(result)
+      // console.log(result)
       break
+    }
+
+    case 'e': {
+      if (!currentExpression) currentExpression = 'e^('
+      else currentExpression += ' * e^( '
+
+      lastAction = action
+      break
+    }
+
+    case 'one-div-x': {
+      if (!currentExpression) currentExpression = '1 / '
+      else currentExpression += ' * (1 / '
+
+      lastAction = action
+      break
+    }
+
     case 'clear-expression':
       currentExpression = ''
       result = 0
       break
+
     case 'clear':
       clearValues()
       break
-    case 'plus-minus':
+
+    case 'plus-minus': {
       if (currentExpression[0] === '-') {
         if (currentExpression.length == 1) currentExpression = ''
         else currentExpression = currentExpression.slice(1)
         break
       }
-      // else if (currentExpression[0] === '-' && currentExpression.length >= 2) {
-      //   currentExpression = currentExpression.slice(1)
-      //   break
-      // }
 
       lastAction = currentExpression
       currentExpression = '-' + lastAction.trim()
       lastAction = action
       break
-    case 'dot':
+    }
+
+    case 'dot': {
       if (!canPutDot) break
       else currentExpression += !currentExpression ? '0' + value : value
       canPutDot = false
       break
-    case '0':
+    }
+
+    case '0': {
       if (currentExpression[0] == '0' && currentExpression[1] != '.') break
 
-      if (!currentExpression) setNumbers(value, action)
-      else setNumbers(value, action)
+      if (!currentExpression) setNumbers(value)
+      else setNumbers(value)
       break
+    }
+
     case 'left-bracket':
     case 'right-bracket':
     case '1':
@@ -129,24 +119,85 @@ const handleClick = (action, value) => {
     case '7':
     case '8':
     case '9':
-      setNumbers(value, action)
+      setNumbers(value)
       break
+
     case 'add-memory':
     case 'sub-memory':
     case 'recall-memory':
     case 'reset-memory':
       handleMemoryChange(action)
       break
+
     default:
       break
   }
 }
 
-function setNumbers(value, action) {
-  if (lastAction === 'equal') {
-    clearValues()
+function handleOperation(action) {
+  symbol = handleActionSymbols(action)
+
+  if (lastAction === 'e') {
+    if (action == 'equal') symbol = ''
+    let eStr = 'e^('
+
+    let startPos = currentExpression.indexOf(eStr) + eStr.length
+    let endPos = currentExpression.indexOf(')', startPos)
+    console.log('startPos', startPos)
+    console.log('endPos', endPos)
+
+    if (startPos == -1) return
+
+    let cutString = currentExpression.slice(startPos, endPos)
+    console.log('cutString', cutString)
+
+    expressionString += `Math.exp(${cutString})` + symbol
+
+    historyExpression += eStr + cutString + ')' + symbol
+
+    if (action == 'equal') historyExpression += ' ='
+
+    currentExpression = ''
     lastAction = action
+    canPutDot = true
+    return
   }
+
+  if (lastAction === 'equal') {
+    historyExpression = result + symbol
+    expressionString = result.toString() + symbol
+    // console.log('expressionString', expressionString)
+
+    currentExpression = ''
+    lastAction = action
+    canPutDot = true
+    return
+  }
+
+  if ((lastAction == 'divide' || lastAction == 'recall-memory') && currentExpression == '0') {
+    // console.log(memory)
+    // console.log(currentExpression)
+    historyExpression += currentExpression + ' ='
+    currentExpression = 'На ноль делить нельзя'
+    expressionString = ''
+    return
+  }
+
+  if (action == 'equal') symbol = ''
+  expressionString += currentExpression + symbol
+
+  historyExpression = expressionString
+
+  if (action == 'equal') historyExpression += ' ='
+
+  currentExpression = ''
+  lastAction = action
+  canPutDot = true
+}
+
+function setNumbers(value) {
+  if (lastAction === 'equal') clearValues()
+
   if (currentExpression === '0') currentExpression = value
   else currentExpression += value
 }
@@ -184,6 +235,8 @@ function handleMemoryChange(action) {
       break
   }
 }
+
+function handleENumber() {}
 
 function handleActionSymbols(action) {
   switch (action) {
